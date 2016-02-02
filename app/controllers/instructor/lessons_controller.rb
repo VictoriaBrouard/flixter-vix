bacd3a99c3cb6c6bcb028ec6727400b124d06591
Victoria Brouard <victoria_brouard@hotmail.co.uk>
2016-02-02 14:11:48 +0000

    Final Permissions

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,24 +1,24 @@
 class Instructor::LessonsController < ApplicationController
 	before_action :authenticate_user!
+	before_action :require_authorized_for_current_section
 
 	def new
-		if current_section.course.user != current_user
-			return render text: 'Unauthorized', status: :unauthorized
-		end
 		@lesson = Lesson.new
 	end
 
 	def create
-		if current_section.course.user != current_user
-			return render text: 'Unauthorized', statys: :unauthorized
-		end
 		@lesson = current_section.lessons.create(lesson_params)
 		redirect_to instructor_course_path(current_section.course)
 	end
 
 	private
 
-	helper_method
+	def require_authorized_for_current_section
+		if current_section.course.user != current_user
+			return render text: 'Unauthorized', status: :unauthorized
+		end
+
+	helper_method :current_section
 	def current_section
 		@current_section ||= Section.find(params[:section_id])
 	end
